96cb66466060cbd6f1822b212f0c2ca6828b4e33
M68864 <M68864@fe80:0:0:0:d05b:74f6:25a4:3c87%11>
2020-08-16 07:53:35 +0900

    200816

--- JavaSample/ifSwitch/switchStatement/Switch.java
+++ JavaSample/ifSwitch/switchStatement/Switch.java
@@ -4,19 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Switch
-{
+public class Switch {
 
-	public static void main(String[] args) throws IOException
-	{
+	public static void main(String[] args) throws IOException {
 		System.out.println("Input integer next line.");
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
 		int res = Integer.parseInt(str);
 
-		switch(res%2)
-		{
+		switch (res % 2) {
 		case 0:
 			System.out.println("Even Number");
 			break;
@@ -27,9 +24,7 @@ public class Switch
 			System.out.println("Odd Number");
 			break;
 
-			//break文が抜けていると全ての条件分岐が実行されてしまうので注意
+		//break文が抜けていると全ての条件分岐が実行されてしまうので注意
 		}
-
 	}
-
 }
