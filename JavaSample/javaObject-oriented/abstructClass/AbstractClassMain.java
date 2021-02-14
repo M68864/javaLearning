ba43498ddf2d8f462d3fba6f01dc0847bae5016c
M68864 <M68864@192.168.0.4>
2021-02-14 09:16:55 +0900

    20210214 modified

--- JavaSample/javaObject-oriented/abstructClass/AbstractClassMain.java
+++ JavaSample/javaObject-oriented/abstructClass/AbstractClassMain.java
@@ -1,77 +1,91 @@
 package abstructClass;
 
-	//抽象クラスの説明と使い方について記述する
+//抽象クラスの説明と使い方について記述する
 
-	//抽象クラスはオブジェクト生成ができないので、サブクラスで定義してオブジェクト生成する
+//抽象クラスはオブジェクト生成ができないので、サブクラスで定義してオブジェクト生成する
 
+/**
+ * 抽象クラス本体
+ *
+ * @author M68864
+ * @since 2021/02/13
+ */
+abstract class AbstractClass {
 
-
-abstract class AbstractClass
-{
 	protected int num1A;
 
-	public void setMethod(int s)
-	{
+	public void setMethod(int s) {
 		num1A = s;
-		System.out.println("setMethod num1A:"+num1A);
+		System.out.println("setMethod num1A:" + num1A);
 	}
 
-	abstract void show();	//抽象クラスにはメソッドの内容が何も書かれていない抽象メソッドが存在する
+	abstract void show(); //抽象クラスにはメソッドの内容が何も書かれていない抽象メソッドが存在する
 }
 
-class Object1 extends AbstractClass
-{
+/**
+ * 抽象クラスを継承した実体クラス
+ *
+ * @author M68864
+ * @since 2021/02/13
+ */
+class EntityClass extends AbstractClass {
+
 	private int num1B;
 	private double num1C;
 
-	public Object1(int n, double g)	//パブリックなコンストラクタ
-	{
+	//パブリックなコンストラクタ
+	public EntityClass(int n, double g) {
 		num1B = n;
 		num1C = g;
-		System.out.println("Object1 constructor num1B:"+num1B+"num1C:"+num1C);
+		System.out.println("EntityClass constructor num1B:" + num1B + "num1C:" + num1C);
 	}
 
-	public void show()
-	{
-		System.out.println("Object1 showMethod num1A:"+num1A+"num1B:"+num1B+"num1C:"+num1C);
+	public void show() {
+		System.out.println("EntityClass showMethod num1A:" + num1A + "num1B:" + num1B + "num1C:" + num1C);
 	}
 	//抽象クラスを拡張したサブクラスはどれも、抽象クラスの抽象メソッド（show()method）と同じ名前のメソッドを必ずもっている
 
 }
 
-class Object2 extends AbstractClass
-{
+/**
+ * 抽象クラスを継承した実体クラス
+ *
+ * @author M68864
+ * @since 2021/02/13
+ */
+class Object2 extends AbstractClass {
+
 	private int num1D;
 
-	public Object2(int f)
-	{
+	public Object2(int f) {
 		num1D = f;
-		System.out.println("Object2 constructor num1D:"+num1D);
+		System.out.println("Object2 constructor num1D:" + num1D);
 	}
 
-	public void show()
-	{
+	public void show() {
 		System.out.println("Object2 show Method");
 	}
 }
 
-public class AbstractClassMain
-{
+/**
+ * 各クラスの検証クラス
+ *
+ * @author M68864
+ * @since 2021/02/13
+ */
+public class AbstractClassMain {
 
-	public static void main(String[] args)
-	{
-		AbstractClass[] ac = new AbstractClass[2];	//抽象クラスの配列を用意する
+	public static void main(String[] args) {
+		AbstractClass[] ac = new AbstractClass[2]; //抽象クラスの配列を用意する
 
-		ac[0] = new Object1(1, 3.14);	//一番目のオブジェクト
+		ac[0] = new EntityClass(1, 3.14); //一番目のオブジェクト
 		ac[0].setMethod(5);
 
-		ac[1] = new Object2(99);	//二番目のオブジェクト
+		ac[1] = new Object2(99); //二番目のオブジェクト
 		ac[1].setMethod(500);
 
-		for(int i=0; i<ac.length; i++)
-		{
+		for (int i = 0; i < ac.length; i++) {
 			ac[i].show();
 		}
 	}
-
 }
--- JavaSample/javaObject-oriented/abstructClass/AbstructClassInstanceofOperator.java
+++ JavaSample/javaObject-oriented/abstructClass/AbstructClassInstanceofOperator.java
@@ -2,22 +2,19 @@ package abstructClass;
 
 //instanceof　を使うとオブジェクトのクラスを調べることが出来る
 
-public class AbstructClassInstanceofOperator
-{
+public class AbstructClassInstanceofOperator {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		AbstractClass[] ac = new AbstractClass[2];
 
-		ac[0] = new Object1(1, 3.1415);
+		ac[0] = new EntityClass(1, 3.1415);
 		ac[1] = new Object2(232);
 
-		for(int i=0; i<ac.length; i++)
-		{
-			if(ac[i] instanceof Object1)
-				System.out.println("No."+(i+1)+"'s object is Object1");
+		for (int i = 0; i < ac.length; i++) {
+			if (ac[i] instanceof EntityClass)
+				System.out.println("No." + (i + 1) + "'s object is Object1");
 			else
-				System.out.println("No."+(i+1)+"'s object is NOT Object1");
+				System.out.println("No." + (i + 1) + "'s object is NOT Object1");
 
 		}
 
